19d3fcea743f3b690b9b7dcf0361bd7c18ad4f93
Cloud84-blip <64501479+Cloud84-blip@users.noreply.github.com>
2023-02-07 18:05:09 +0100

    TextFile

--- src/fr/uca/jgit/model/TextFile.java
+++ src/fr/uca/jgit/model/TextFile.java
@@ -1,25 +1,72 @@
 package fr.uca.jgit.model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.security.MessageDigest;
+
 public class TextFile implements Node {
     private String content;
 
     @Override
     public String hash() {
-        return null;
+        StringBuffer sb = new StringBuffer();
+        try{
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(content.getBytes());
+            byte[] bytes = md.digest();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 
     /** Stores the corresponding object in .git directory (to file .git/object/[hash]). **/
     @Override
     public void store() {
+        try {
+            String hash = hash();
+            FileOutputStream fos = new FileOutputStream(".git/objects/" + hash);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(this);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /** Loads the text file corresponding to the given hash (from file .git/object/[hash]). **/
     public static TextFile loadFile(String hash) {
-        return null;
+        try {
+            FileInputStream fis = new FileInputStream(".git/objects/" + hash);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            TextFile textFile = (TextFile) ois.readObject();
+            ois.close();
+            fis.close();
+            return textFile;
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /** Restores the file node at the given path. **/
     @Override
     public void restore(String path) {
+        try {
+            File file = new File(path);
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(content);
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
